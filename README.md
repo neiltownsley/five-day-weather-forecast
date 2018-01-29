df7c6dc57cdcd5feb9ab709f73074511731f0c2d
neiltownsley <n31LT0wn5l3y>
2018-01-29 09:44:20 +0000

    update readme, add base component spec

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-# FiveDayWeatherForecast
+# Five Day Weather Forecast
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.3.
+## Application Information
 
-## Development server
+The application utilises Open Weather Map API to retrieve a five day weather forecast summary.
+The daily summary is based on the retrieved OWM properties for the given day at 12:00 pm.
+The raw data is mapped to an application specific Model which is used to display the dashboard data.
+
+## Problems
+ * Retrieving the data and mapping the required values
+
+## Solutions
+ * After retrieving the raw OWM data via a http get request the data is mapped to a model via a service which is called after executing the http request. (see: open.weather.map.response.mapper.ts)
+
+## Improvements
+ * Currently the dashboard on displays a daily summary based on the OWM data for the given day at 12:00pm, it would be nice to expand this to a morning, afternoon evening summary.
+
+## Instructions
+
+
+Run `git clone https://github.com/neiltownsley/five-day-weather-forecast.git` to clone 
+
+Run `npm i` to install the dependencies
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
@@ -23,5 +41,7 @@ Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
 ## Further help
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.3.
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/weather-dashboard/weather-dashboard.component.spec.ts
+++ src/app/weather-dashboard/weather-dashboard.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
-import { WeatherDashboardComponent } from './weather-dashboard.component';
+import {WeatherDashboardComponent} from './weather-dashboard.component';
 import {WeatherItemListComponent} from './weather-item-list/weather-item-list.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {WeatherIconsModule} from 'ngx-icons';
@@ -8,6 +8,9 @@ import {OpenWeatherMapRequestHandler} from '../shared/open.weather.map.request.h
 import {HttpClientModule} from '@angular/common/http';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {OpenWeatherMapResponseMapper} from '../shared/open.weather.map.response.mapper';
+import {Observable} from "rxjs/Observable";
+import {MockOpenWeatherMapHttpResponse} from "../test/mock.open.weather.map.http.response";
+import {OpenWeatherMap} from "../shared/open.weather.map.item";
 
 describe('WeatherDashboardComponent', () => {
   let component: WeatherDashboardComponent;
@@ -15,12 +18,12 @@ describe('WeatherDashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WeatherDashboardComponent, WeatherItemListComponent ],
+      declarations: [WeatherDashboardComponent, WeatherItemListComponent],
       providers: [OpenWeatherMapRequestHandler, OpenWeatherMapResponseMapper],
       imports: [RouterTestingModule, WeatherIconsModule, HttpClientModule,
         HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -32,4 +35,23 @@ describe('WeatherDashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('ngOnInit, should retrieve open weather map api data,' +
+    ' and make it available to child component via async input property',
+    async(inject([OpenWeatherMapRequestHandler],
+      (openWeatherMapRequestHandler: OpenWeatherMapRequestHandler) => {
+        spyOn(openWeatherMapRequestHandler, 'getOpenWeatherMapFiveDayForecast').and.callFake(
+          () => {
+            return Observable.of(MockOpenWeatherMapHttpResponse.getMappedOpenWeatherMapResponse());
+          });
+        component.ngOnInit();
+        fixture.detectChanges();
+
+        component.openWeatherMap.subscribe(
+          (openWeatherMap: OpenWeatherMap) => {
+            expect(openWeatherMap).toEqual(MockOpenWeatherMapHttpResponse.getMappedOpenWeatherMapResponse());
+          });
+      })
+    )
+  );
 });
